eb89722860476abecda366a8cd43226d5731b242
Charly <charlyfb90@gmail.com>
2017-07-27 09:43:20 +0200

    Update http_test.go

--- http_test.go
+++ http_test.go
@@ -7,14 +7,14 @@ import (
 
 const signedUrl = "http://webservices.amazon.es/onca/xml?AWSAccessKeyId=AWSK&AssociateTag=collectus-21&Keywords=Clean+Code&Operation=ItemSearch&ResponseGroup=Images%2CItemAttributes&SearchIndex=Books&Service=AWSECommerceService&Signature=%2FBe%2BqvyOpvdc2XA0YCxPYD3Wp1LKqWDBnk60k3%2BLPlM%3D&Timestamp=2017-07-26+23%3A00%3A00+%2B0900+Europe%2FSpain&Version=2013-08-01"
 
-func TestHttpClientSignUrl(t *testing.T) {
+func TestHTTPClientSignUrl(t *testing.T) {
 	client := Client{"AWSK", "AWSS", "collectus-21", "ES"}
 	params := map[string]string{
 		"SearchIndex":   "Books",
 		"Keywords":      "Clean Code",
 		"ResponseGroup": "Images,ItemAttributes",
 	}
-	httpClient := HttpClient{client, params}
+	httpClient := HTTPClient{client, params}
 
 	urlValues := httpClient.buildUrlValues()
 	urlValues.Set("Timestamp", time.Date(2017, time.July, 26, 23, 00, 0, 0, time.FixedZone("Europe/Spain", 9*60*60)).String())
@@ -28,14 +28,14 @@ func TestHttpClientSignUrl(t *testing.T) {
 	}
 }
 
-func TestHttpClientDo(t *testing.T) {
+func TestHTTPClientDo(t *testing.T) {
 	client := Client{"AWSK", "AWSS", "collectus-21", "ES"}
 	params := map[string]string{
 		"SearchIndex":   "Books",
 		"Keywords":      "Clean Code",
 		"ResponseGroup": "Images,ItemAttributes",
 	}
-	httpClient := HttpClient{client, params}
+	httpClient := HTTPClient{client, params}
 
 	res, err := httpClient.Do() // TODO MOCK THE CALL TO AMAZON
 	if err != nil {
