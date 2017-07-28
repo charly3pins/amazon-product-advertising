e2dcbb6c2bdd7348ced2de1b5ed9e51cd5bd28a9
Carles Fusté <charlyfb90@gmail.com>
2017-07-28 13:43:15 +0200

    separate client and client config and use httpclient interface

--- client.go
+++ client.go
@@ -2,27 +2,53 @@ package amazon
 
 import (
 	"encoding/xml"
+	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
 )
 
-type Client struct {
+type ClientConfig struct {
 	AccessKeyID     string
 	SecretAccessKey string
 	AssociateTag    string
 	Region          string
 }
 
-func (c Client) ItemSearch(searchIndex, keywords string) (*ItemSearchResponse, error) {
+type Client interface {
+	ItemSearch(string, string) (*ItemSearchResponse, error)
+}
+
+func NewClient(config ClientConfig) Client {
+	return client{config.Region, NewAWSHTTPClient(config)}
+}
+
+type client struct {
+	region string
+	client HTTPClient
+}
+
+func (c client) ItemSearch(searchIndex, keywords string) (*ItemSearchResponse, error) {
 	itemSearchResponse := ItemSearchResponse{}
 	params := map[string]string{
 		"SearchIndex":   searchIndex,
 		"Keywords":      keywords,
 		"ResponseGroup": "Images,ItemAttributes",
 	}
-	httpClient := HTTPClient{c, params}
-	res, err := httpClient.Do()
+
+	endpoint := fmt.Sprintf("http://%s/onca/xml", domains[c.region])
+	parsedURL, _ := url.Parse(endpoint)
+	req, err := http.NewRequest("GET", parsedURL.String(), nil)
+	if err != nil {
+		log.Println("Error:", err.Error())
+		return nil, err
+	}
+
+	res, err := c.client.Do(req, params)
 	if data, _ := ioutil.ReadAll(res.Body); data != nil {
 		if err = xml.Unmarshal(data, &itemSearchResponse); err != nil {
+			log.Println("Error:", err.Error())
 			return nil, err
 		}
 	}
